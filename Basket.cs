168253caaa39e0872d7553c79455fc48a791a34a
JaydenWO <13038146@imail.sunway.edu.my>
2016-09-28 20:09:36 +0800

    Add files via upload

---
+++ Basket.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Basket 
+{
+
+    private static List<Fruits> fruitList = new List<Fruits>();
+    private static List<Fruits> fruitList1 = new List<Fruits>();
+    
+
+    public void AddFruit()
+    {
+        for (int i=0; i<3; i++)
+        {
+            Fruits apple = new Fruits("apple", "red", 300, 800);
+            fruitList.Add(apple);
+            
+        }
+
+       for (int i=0; i<6; i++)
+        {
+            Fruits lemon = new Fruits("lemon", "yellow", 100, 600);
+            fruitList.Add(lemon);
+            
+        }
+
+       for (int i=0; i<3; i++)
+        {
+            Fruits watermelon = new Fruits("watermelon", "green", 300, 1000);
+            fruitList.Add(watermelon);
+        }
+    }
+
+    public int DiscardFruit(string name, int weight)
+    {
+        int count = 0;
+        foreach (Fruits fruit in fruitList)
+        {
+            if (fruit.FruitName != name && fruit.FruitWeight < weight)
+            {
+                fruitList.Remove(fruit);
+                count++;
+            }
+        }
+        return count;
+        //fruitList.RemoveAll(x => x.FruitName != name && x.FruitWeight < weight);
+    }
+
+    public List<Fruits> GetFruitType(string name)
+    {
+         fruitList1= fruitList;
+        fruitList1.RemoveAll(fruit => fruit.FruitName != name);
+        return fruitList1;
+    }
+	
+}
---
+++ GameManager.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+public class GameManager : MonoBehaviour 
+{
+    private Basket myBasket;
+    private System.Random rand = new System.Random();
+    private List<GameObject> appleObject = new List<GameObject>();
+    private List<GameObject> lemonObject = new List<GameObject>();
+    private List<GameObject> watermelonObject = new List<GameObject>();
+    private List<Fruits> fruitList1 = new List<Fruits>();
+    public InputField appleInputField, lemonInputField, watermelonInputField;
+    public int removeCount;
+
+    public void OnClickDiscard()
+    {
+        if (appleInputField.text != "")
+        {
+            int appleWeight = Convert.ToInt32(appleInputField.text);
+            removeCount=myBasket.DiscardFruit("apple", appleWeight);
+            for (int x = 0; x < removeCount; x++)
+            {
+                Destroy(appleObject[x]);
+            }
+        }
+       
+        if (lemonInputField.text != "")
+        {
+            int lemonWeight = Convert.ToInt32(lemonInputField.text);
+            removeCount = myBasket.DiscardFruit("lemon", lemonWeight);
+            for (int x = 0; x < removeCount; x++)
+            {
+                Destroy(appleObject[x]);
+            }
+        }
+        
+        if (watermelonInputField.text != "")
+        {
+            int watermelonWeight = Convert.ToInt32(watermelonInputField.text);
+            removeCount = myBasket.DiscardFruit("watermelon", watermelonWeight);
+            for (int x = 0; x < removeCount; x++)
+            {
+                Destroy(appleObject[x]);
+            }
+        }
+    }
+	
+    public void CreateObject(string fruitType,List <GameObject> fruitObjectList,List<Fruits> fruitList)
+    {
+        //fruitList= myBasket.GetFruitType(fruitType);
+        foreach (Fruits fruit in myBasket.GetFruitType(fruitType))
+        {
+           
+            GameObject fruitObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            fruitObject.transform.position = new Vector3((float)rand.NextDouble() * 16 - 8, (float)rand.NextDouble() * 6 - 3, 0);
+            
+            Material material = new Material(fruitObject.GetComponent<Renderer>().material);
+            material.EnableKeyword("_EMISSION");
+            if(fruit.FruitColor=="red")
+            {
+                material.SetColor("_EmissionColor", Color.red);
+            }
+            else if(fruit.FruitColor=="green")
+            {
+                 material.SetColor("_EmissionColor", Color.green);
+            }
+            else
+            {
+                 material.SetColor("_EmissionColor", Color.yellow);
+            }
+
+            fruitObject.GetComponent<Renderer>().material = material;
+            fruitObjectList.Add(fruitObject);
+        }
+    }
+
+    public void RemoveAll()
+    {
+        foreach (GameObject fruit in appleObject)
+        {
+            Destroy(fruit);
+        }
+        appleObject.Clear();
+        
+        
+        foreach (GameObject fruit in lemonObject)
+        {
+            Destroy(fruit);
+        }
+        lemonObject.Clear();
+       
+        foreach (GameObject fruit in watermelonObject)
+        {
+            Destroy(fruit);
+        }
+        watermelonObject.Clear();
+      }
+
+    public void onClickRestart()
+    {
+        Restart();
+    }
+
+    public void Restart()
+    {
+        RemoveAll();
+        myBasket = new Basket();
+        myBasket.AddFruit();
+
+        CreateObject("apple", appleObject, fruitList1);
+        CreateObject("lemon", lemonObject, fruitList1);
+        CreateObject("watermelon", watermelonObject, fruitList1);
+    }
+
+    void Start()
+    {
+        Restart();
+    }
+
+
+    void Update()
+    {
+
+    }
+    
+
+
+}
